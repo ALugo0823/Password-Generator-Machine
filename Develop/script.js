3ce1c81232884ce617de628ed2b57ca36957aca6
lugo.kse23@gmail.com <lugo.kse23@gmail.com>
2023-04-06 01:43:12 -0700

    added logic to the webpage with JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,76 @@
+//Arrays of possible characters to be generated
+var nums = [0,1,2,3,4,5,6,7,8,9];
+var specialChar = ["-","!", "#", "$", "%", "^", "&", "*", ";" , "<" , ">" , "?"];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+// variable declarations of user inputs
+var userLength;
+var userChoice = [];
+var userSpecialChar;
+var userLowerCase;
+var userUpperCase;
+var userNum;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt which asks user how many characters they want password to be (8-128 chars)
+function promptUser() {
+  userChoice = [];
+  userLength = (prompt("How many characters would you like your password to have?"));
+    while (userLength < 8 || userLength > 128) {
+      alert("Password must be a number between 8-128. Please try again")
+       return promptUser();
+    }
+
+userSpecialChar = confirm("Would you like special characters in your password?");
+userLowerCase = confirm("Would you like lowercase letters in your password?");
+userUpperCase = confirm("Would you like uppercase letters in your password?");
+userNum = confirm("Would you like numbers in your password?");
+
+//loop that checks if all parameters are set to false
+while(userSpecialChar === false && userLowerCase === false && userUpperCase === false && userNum === false) { 
+  alert("Password must meet at least one of the parameters. Please try again.");
+  userSpecialChar = confirm("Would you like special characters in your password?");
+  userLowerCase = confirm("Would you like lowercase letters in your password?");
+  userUpperCase = confirm("Would you like uppercase letters in your password?");
+  userNum = confirm("Would you like numbers in your password?");
+}
+    //conditional statements which check for user input on prompts (true or false)
+    if (userSpecialChar) {
+       userChoice = userChoice.concat(specialChar);
+}
+    if (userLowerCase) {
+    userChoice = userChoice.concat(lowerCase);
+}
+    if (userUpperCase) {
+    userChoice = userChoice.concat(upperCase);
+}
+    if (userNum) {
+    userChoice = userChoice.concat(nums); 
+  }
+  return true;
+}
+//function which loops through an iteration to generate random characters based on user confirmations
+    function generatePassword() {
+      var password = "";
+      for(var i = 0; i < userLength; i++) {
+        var randomChar = Math.floor(Math.random() * userChoice.length);
+        password = password + userChoice[randomChar];
+      }
+      return password;
+    }
+
+// Write password to the #password input
+function writePassword() {
+  var promptIsCorrect = promptUser();
+  var passwordText = document.querySelector("#password");
+  if(promptIsCorrect) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Password-Generator-Machine
+## Description
+This program was made to generate a random password based on the user's selections. The selections determines if the user would like their password
+to have uppercase letters, lowercase letters, special characters, or numbers. The password does have length parameters of 8-128 characters of length.
+The logic of this program was created using JavaScript.
+
+## Installation
+N/A
+
+## Usage
+Users only need a computer, mobile device, or tablet with an internet connection.
+Password Generator URL: 
+
+![Password Generator Screenshot](password-generator-screenshot.png)
+
+
+## Credits
+Tutorials used:
+1. https://www.w3schools.com/jsref/met_win_confirm.asp
+2. https://www.w3schools.com/jsref/met_win_alert.asp
+3. https://www.w3schools.com/jsref/jsref_random.asp
+
+## License
+MIT License
